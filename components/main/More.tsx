f1e30e2b50c7c6bddd98a5f96991079af4277320
mwaniki-david <111046198+mwaniki-david@users.noreply.github.com>
2024-01-22 15:29:55 +0300

    iiiii
    sa

--- components/main/More.tsx
+++ components/main/More.tsx
@@ -21,7 +21,7 @@ const More = () => {
         <div className='py-4 px-2 mt-[30px] text-center max-w-[450px] md:max-w-[750px] '>
             <h1 className='items-center justify-center  uppercase underline underline-offset-8 text-[35px] md:text-[65px] font-bold'>more offers</h1>
             <h2 className=' text-[25px] uppercase'>@ nice prices</h2>
-            <p className=' text-1xl'>Whether you're a solo artist, a small team, or a larger group, our array of specialized studios ensures that every creative endeavor finds its ideal space. Immerse yourself in an environment that supports your artistic journey and unlocks the full potential of your projects. contact us via tel:0711023728 for any special request.Here is an example of our editting  work</p>
+            <p className=' text-1xl'>Whether you are a solo artist, a small team, or a larger group, our array of specialized studios ensures that every creative endeavor finds its ideal space. Immerse yourself in an environment that supports your artistic journey and unlocks the full potential of your projects. contact us via tel:0711023728 for any special request.Here is an example of our editting  work</p>
             </div> 
             <div className=' '>
               <YouTube videoId="aHfqM5MpJiQ" opts={opts} onReady={onPlayerReady} className=' md:max-w-[800px]' />
