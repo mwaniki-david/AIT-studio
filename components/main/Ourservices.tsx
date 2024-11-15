aa7722249b5c62c124fb10b16657d89faa5ed7a9
mwaniki-david <111046198+mwaniki-david@users.noreply.github.com>
2024-11-15 14:56:00 +0300

    big problem

--- components/main/Ourservices.tsx
+++ components/main/Ourservices.tsx
@@ -12,7 +12,7 @@ const ourservices = () => {
 
           <div className="flex flex-col  gap-3 justify-center item-center">
             <div className=" px-[5px] ">
-              <h1 className="uppercase mt-10 text-[30px] md:text-[35px] lg:text-[42px] max-w-[600px] md:max-w-[700px] font-bold cursor: pointer">
+              <h1 className="uppercase mt-14 text-[30px] md:text-[35px] lg:text-[42px] max-w-[600px] md:max-w-[700px] font-bold cursor: pointer">
                 your one-stop destination for all things socialmedia!!
               </h1>
               <h2 className=" md:px-5  text-[12px] md:text-[15px]  max-w-[500px] md:max-w-[660px]  ">
@@ -23,7 +23,7 @@ const ourservices = () => {
                 true crative potential.
               </h2>
               <h3 className='text-[13px] md:text-[25px] px-1 '>The right price for your creativity.</h3>
-              <ul className="flex flex-row text-base md:text-1xl justify-start items-start px-4 py-2 gap-12">
+              <ul className="flex flex-row text-base md:text-1xl justify-start items-start px-4 py-2 gap-12 mt-2">
                 <div className="font-bold">
                 <p className="flex flex-row">
                 <MdArrowCircleRight  className="mt-1 w-6"/>top job
